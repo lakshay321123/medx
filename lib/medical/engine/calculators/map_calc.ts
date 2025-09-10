c719ad906330d86787a4c37da8be64486cc26dee
lakshay321123 <lakshay321@gmail.com>
2025-09-10 14:24:18 +0530

    Update map_calc.ts

--- lib/medical/engine/calculators/map_calc.ts
+++ lib/medical/engine/calculators/map_calc.ts
@@ -1,9 +1,16 @@
 /**
- * Mean arterial pressure (MAP) from SBP and DBP:
+ * Mean Arterial Pressure (mmHg)
  * MAP = (SBP + 2*DBP) / 3
  */
-export interface MAPInput { sbp_mmHg: number; dbp_mmHg: number; }
-export interface MAPResult { map_mmHg: number; }
+export interface MAPInput {
+  sbp_mmHg: number;
+  dbp_mmHg: number;
+}
+export interface MAPResult {
+  map_mmHg: number;
+}
+
 export function runMAP(i: MAPInput): MAPResult {
-  return { map_mmHg: (i.sbp_mmHg + 2*i.dbp_mmHg) / 3 };
+  const map = (i.sbp_mmHg + 2 * i.dbp_mmHg) / 3;
+  return { map_mmHg: map };
 }
