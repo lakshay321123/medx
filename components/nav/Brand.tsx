137f4cc9eec6ab38ee3d36256530d40f602c2726
lakshay321123 <lakshay321@gmail.com>
2025-09-16 01:24:45 +0530

    Update Brand.tsx

--- components/nav/Brand.tsx
+++ components/nav/Brand.tsx
@@ -1,22 +1,5 @@
-"use client";
-
-import { useCallback } from "react";
 import Logo from "@/components/brand/Logo";
 
 export default function Brand() {
-  const handleClick = useCallback(() => {
-    if (typeof window !== "undefined") {
-      try {
-        sessionStorage.removeItem("search_docked");
-      } catch {
-        // ignore storage access issues
-      }
-    }
-  }, []);
-
-  return (
-    <span onClick={handleClick} className="inline-flex">
-      <Logo />
-    </span>
-  );
+  return <Logo />;
 }
